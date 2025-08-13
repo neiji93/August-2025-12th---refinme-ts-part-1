484115990724eea576ae09e4a858bc5e07ea60c6
Nejdet ALTINISIK <nejdet.altinisik@yahoo.fr>
2025-08-13 09:04:40 +0200

    Create CorrespondingNavierStokesRefinmentsclasses.h

---
+++ Chapterone/CorrespondingNavierStokesRefinmentsclasses.h
@@ -0,0 +1,15 @@
+//as we deal with inclusive sets, we can 
+//not refine T term. then lets refine viscosity by
+//separating two petri sets. nn can be used
+//but it would be better to get the bottlenecks on computational
+//energy terms. this will prevent using bots.
+//so the refinment is possible, i thin'k.
+
+class refinmentnonNnPushViscosityInPetriEts
+{
+public:
+
+ //physics : 3 or 5 TeV is enough in beam physics maybe to prevent
+ //using models but not bots.
+ void...
+}
